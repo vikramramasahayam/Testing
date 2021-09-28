e420160cac8fe8babf741e9a29a6518dabae4bc6
Vikram Ramasahayam <vramasahayam@ptc.com>
2021-09-28 09:55:54 -0400

    Adding Builder test

---
+++ src/com/test/builder/Student.java
@@ -0,0 +1,16 @@
+package com.test.builder;
+
+public class Student {
+
+    public String firstName;
+    public String lastName;
+
+    public static class Builder {
+        private String fName;
+        private String lName;
+
+        public Builder() {}
+
+    }
+
+}
---
+++ src/com/test/builder/StudentTest.java
@@ -0,0 +1,11 @@
+package com.test.builder;
+
+public class StudentTest {
+
+    public static void main(final String[] args) {
+
+        // final Student student = new Builder().addFirstName("larry").addLastName("king").build();
+
+    }
+
+}
